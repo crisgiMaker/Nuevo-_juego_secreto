08c1cfe63f94f5cbf2858a2cef98864bd4e5c85f
Cristian Alvaro Martinez Garcia <90767940+crisgiMaker@users.noreply.github.com>
2024-07-06 20:27:13 -0400

    Update app.js
    mejora

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ function asignarTextoElemento(elemento, texto) {
 function verificarIntento () {
     let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
 
-    if(numeroUsuario === numeroSecreto){
+    if(numeroUsuario === numeroSecreto) {
         asignarTextoElemento('p', `acertaste el numero en ${intentos} ${(intentos === 1)  ? 'vez' : 'veces'}`);
         document.getElementById('reiniciar').removeAttribute('disabled');
     }else{
         //el usuario no acerto
-        if(numeroUsuario > numeroSecreto){
+        if(numeroUsuario > numeroSecreto) {
             asignarTextoElemento('p', 'El numero secreto es menor');
         }else{
             asignarTextoElemento('p', 'El numero secreto es mayor');
@@ -30,14 +30,14 @@ function verificarIntento () {
 }
 
 function limpiarCaja(){
-    document.querySelector('#valorUsuario'). value = ''; //la forma reducida es de esta manera
+    document.querySelector('#valorUsuario').value = ''; //la forma reducida es de esta manera
     //valorCaja = ''; tambien peude ser de esta forma
 }
 
 function generarNumeroSecreto() {
     let numeroGenerado = Math.floor(Math.random()*numeroMax)+1;
 //si generamos el numero sorteado numero maximo
-    if (listaNumeroSorteados.length == numeroMax){
+    if (listaNumeroSorteados.length == numeroMax) {
         asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles');
     }else{
 
